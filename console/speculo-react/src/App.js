b625248a3406f782ebd057fa8669d378f8f5ea3b
Visal Rajapakse <visalrajapakse@gmail.com>
2020-01-29 21:16:29 +0530

    webcam snapshot component implemented

---
+++ console/speculo-react/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./App.css";
+import {WebcamCapture} from "./components/webcam/webcam.component";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <WebcamCapture />
+      </div>
+    );
+  }
+}
+
+export default App;
