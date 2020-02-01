59cd815a0d80bed46d050f26f49f8b4c1a708e4b
Isala Piyarisi <code@isala.me>
2020-02-01 19:04:11 +0530

    Proof of Concept

--- fingerprinter/app.py
+++ fingerprinter/app.py
@@ -1,21 +1,36 @@
-import tensorflow as tf
-import efficientnet.keras as efn
-from efficientnet.keras import center_crop_and_resize, preprocess_input
-import numpy as np
-import cv2
+from model import Speculo
+from aiohttp import web
+import json
+from PIL import Image
+from io import BytesIO
+import base64
 
-model = efn.EfficientNetB7(weights='imagenet')
-cap = cv2.VideoCapture(0)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+async def predict(request):
+    try:
+        try:
+            data = await request.json()
+        except:
+            return web.Response(body=json.dumps({'error': 'this is not a fucking json request'}),
+                                status=400, content_type='application/json')
+        if 'image' not in data.keys():
+            return web.Response(body=json.dumps({'error': 'send the image you dum fuck'}),
+                                status=400, content_type='application/json')
+        try:
+            image = Image.open(BytesIO(base64.b64decode(data['image'])))
+        except:
+            return web.Response(body=json.dumps({'error': 'i said BASE64 image what the fuck is this ?'}),
+                                status=400, content_type='application/json')
+        return web.Response(body=json.dumps({'data': speculo.predict(image)}),
+                            status=400, content_type='application/json')
+    except Exception as e:
+        print(e)
+        return web.Response(body=json.dumps({'error': 'isala messed something up 🤦‍♂️'}),
+                            status=400, content_type='application/json')
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+app = web.Application()
+app.add_routes([web.post('/', predict)])
+if __name__ == '__main__':
+    speculo = Speculo()
+    web.run_app(app)
