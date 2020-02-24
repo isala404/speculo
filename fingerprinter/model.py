3e3e4561e3a031ee910a3a013165ddb0e74037d4
Isala Piyarisi <code@isala.me>
2020-02-24 11:18:51 +0530

    Model v2

--- fingerprinter/model.py
+++ fingerprinter/model.py
@@ -4,14 +4,22 @@ import numpy as np
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
 from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Dropout
 from tensorflow.keras.models import Model
-from datetime import datetime
+from tensorflow.keras.optimizers import Adam
+
 
 class Speculo:
     def __init__(self):
         self.image_size = (96, 96)
+        self.optimizer = 'adam'
+        self.loss_function = 'mae'
         self.input_img = Input(shape=(96, 96, 3))
         self.encoded = self.encoder()
         self.decoded = self.decoder()
+
+        model_number = 1
+        if os.path.isdir("logs"):
+            model_number = len(os.listdir("logs/")) + 1
+        self.name = f"v{model_number}-{self.optimizer}-{self.loss_function}"
 
     def encoder(self):
         x = Conv2D(72, (3, 3), activation='relu', padding='same')(self.input_img)
@@ -23,8 +31,6 @@ class Speculo:
         x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
         x = MaxPooling2D((2, 2), padding='same')(x)
         # x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-        # x = MaxPooling2D((2, 2), padding='same')(x)
-        # x = Conv2D(4, (3, 3), activation='relu', padding='same')(x)
         # x = MaxPooling2D((2, 2), padding='same')(x)
         x = Dense(512, activation='relu')(x)
         x = Dropout(0.4)(x)
@@ -50,7 +56,8 @@ class Speculo:
 
     def autoencoder(self):
         autoencoder = Model(self.input_img, self.decoded)
-        autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', 'binary_crossentropy', 'mae'])
+        autoencoder.compile(optimizer=Adam(lr=1e-3), loss=self.loss_function,
+                            metrics=['accuracy', 'binary_crossentropy', 'mae'])
         return autoencoder
 
     def _load_image_set(self, directory):
@@ -61,9 +68,12 @@ class Speculo:
             if person_dir == "Front":
                 continue
             else:
-                y_image = Image.open(f"dataset/processed/{directory}/Front/{fronts[i - 1]}").resize(self.image_size, Image.ANTIALIAS)
+                y_image = Image.open(f"dataset/processed/{directory}/Front/{fronts[i - 1]}").resize(self.image_size,
+                                                                                                    Image.ANTIALIAS)
                 for image in os.listdir(f"dataset/processed/{directory}/{person_dir}"):
-                    x.append(np.array(Image.open(f"dataset/processed/{directory}/{person_dir}/{image}").resize(self.image_size, Image.ANTIALIAS)))
+                    x.append(np.array(
+                        Image.open(f"dataset/processed/{directory}/{person_dir}/{image}").resize(self.image_size,
+                                                                                                 Image.ANTIALIAS)))
                     y.append(np.array(y_image))
         return np.reshape(x, [-1, 96, 96, 3]), np.reshape(y, [-1, 96, 96, 3])
 
@@ -75,12 +85,10 @@ class Speculo:
     def train(self):
         x_train, y_train, x_test, y_test = self._create_dataset()
         model = self.autoencoder()
-        model_number = 1
-        if os.path.isdir("logs"):
-            model_number = len(os.listdir("logs/"))+1
 
-        checkpoint = ModelCheckpoint("models/model.h5", monitor='loss', verbose=1, save_best_only=True, mode='min')
-        tensorboard = TensorBoard(log_dir=f'logs/{model_number}-{datetime.now()}', histogram_freq=0, write_graph=False)
+        checkpoint = ModelCheckpoint(f"models/{self.name}.h5", monitor='loss', verbose=1, save_best_only=True,
+                                     mode='min')
+        tensorboard = TensorBoard(log_dir=f'logs/{self.name}', histogram_freq=0, write_graph=False)
 
         model.fit(x_train, y_train,
                   epochs=100,
@@ -89,9 +97,10 @@ class Speculo:
                   validation_data=(x_test, y_test),
 
                   callbacks=[checkpoint, tensorboard])
-        model.save("models/Final-Model.h5")
+
+        model.save(f"models/{self.name}.h5")
 
 
 speculo = Speculo()
-# print(speculo.autoencoder().summary())
+print(speculo.autoencoder().summary())
 speculo.train()
