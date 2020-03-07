d28ca5a6f7a8d901f02403921073024f756c24e7
Isala Piyarisi <code@isala.me>
2020-03-08 00:31:01 +0530

    Updated Inference Script

--- examples/tf-serving-webcam/inference.py
+++ examples/tf-serving-webcam/inference.py
@@ -10,24 +10,27 @@ yolo = YOLO(draw=False, debug=False)
 SIZE = 192
 FINGERPRINT_SIZE = (64, 64, 1)
 
-speculo = Speculo()
-
 known_face_encodings = []
 known_face_names = []
-if not os.path.isdir("faces"):
-    os.mkdir("faces")
+speculo = Speculo(model_path="/mnt/hdd/Projects/SDGP/Speculo/fingerprinter/models/1/Model-v1.h5", visualize=False)
 
-for file in os.listdir("faces"):
-    if os.path.isdir(os.path.join("faces", file)):
-        name = file.title()
-        for file_2 in os.listdir(os.path.join("faces", file)):
-            face_encoding = speculo.predict(speculo.read_image("faces/" + os.path.join(file, file_2)))
-            known_face_encodings.append(face_encoding)
-            known_face_names.append(name)
-    else:
-        face_encoding = speculo.predict(speculo.read_image("faces/" + file))
+print("Processing Faces ....")
+
+for person_dir in os.listdir("faces"):
+    for image in os.listdir(os.path.join("faces", person_dir)):
+        im = cv2.imread(os.path.join("faces", person_dir, image))
+        im = cv2.resize(im, (512, 512), interpolation=cv2.INTER_AREA)
+        boxes = yolo.detect_image_fast(im)
+        if len(boxes) >= 2:
+            print(os.path.join("faces", person_dir, image), "Found more than one face, skipping .....")
+            continue
+        top, left, bottom, right = boxes[0]
+        face = im[int(top):int(bottom), int(left):int(right)]
+        face = cv2.resize(face, FINGERPRINT_SIZE[:2], interpolation=cv2.INTER_AREA)
+        face_encoding = speculo.predict(face)
         known_face_encodings.append(face_encoding)
-        known_face_names.append("".join(file.split(".")[:-1]).title())
+        known_face_names.append(person_dir.title())
+        del im, face, face_encoding
 
 
 def best_match(f_encoding):
@@ -37,6 +40,7 @@ def best_match(f_encoding):
     return known_face_names[int(np.argmin(distances))]
 
 
+print("Start Detecting .... ")
 video_capture = cv2.VideoCapture(0)
 
 while True:
@@ -45,17 +49,20 @@ while True:
     small_frame = cv2.resize(frame, (SIZE, SIZE))
     boxes = yolo.detect_image_fast(small_frame)
     for top, left, bottom, right in boxes:
-        top = int(top * height / SIZE)
-        right = int(right * width / SIZE)
-        bottom = int(bottom * height / SIZE)
-        left = int(left * width / SIZE)
+        try:
+            top = int(top * height / SIZE)
+            right = int(right * width / SIZE)
+            bottom = int(bottom * height / SIZE)
+            left = int(left * width / SIZE)
 
-        face = frame[top:bottom, left:right]
-        face = cv2.resize(face, FINGERPRINT_SIZE[:2], interpolation=cv2.INTER_AREA)
+            face = frame[top:bottom, left:right]
+            face = cv2.resize(face, FINGERPRINT_SIZE[:2], interpolation=cv2.INTER_AREA)
 
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, best_match(speculo.predict(face)), (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, best_match(speculo.predict(face)), (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        except Exception:
+            continue
 
     cv2.imshow('Video', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
