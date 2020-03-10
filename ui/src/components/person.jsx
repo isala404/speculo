2999b82c7369ab1cd22c1427abc42500cee26e4d
Dinuka <drpiyadigama@gmail.com>
2020-03-11 00:22:27 +0530

    Basic UI functionality for one time-stamp per person impelemeneted

---
+++ ui/src/components/person.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Person extends Component{
+    state={};
+
+    render(){
+        return(
+            <h1>This is a Person</h1>
+            )
+    }
+}
+
+export default Person;
