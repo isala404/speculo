e445e3ba16705f3f2478e7a5ddfeb34e86f7f7e5
akassharjun <akassharjun@gmail.com>
2020-03-12 11:28:49 +0530

    changed db name

--- api/auth/config/database.js
+++ api/auth/config/database.js
@@ -1,6 +1,6 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost/node_rest_api';
+const mongoDB = 'mongodb://localhost/speculo';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
