119887f72e4698484d2b303ad815b9cf776896f0
Dinuka <drpiyadigama@gmail.com>
2020-03-14 11:23:05 +0530

    Implemented displaying multiple timestamps for a selected person

--- ui/src/components/dashboard.jsx
+++ ui/src/components/dashboard.jsx
@@ -3,55 +3,27 @@ import videojs from 'video.js';
 import 'video.js/dist/video-js.css';
 import '@videojs/themes/dist/fantasy/index.css';
 import TimeCard from "./timeCard";
+import Person from "./person";
 
 export default class DashBoard extends Component{
-    state={
-        allDetections: [
-          {id: 1, faceName: "Akassh", timeStamp: 60},
-          {id: 2, faceName: "Visal", timeStamp: 1000},
-          {id: 2, faceName: "Nisal", timeStamp: 100}
-        ]     // stores all the detected faces with the timestamps  ~ one time stamp each per person for now
+  constructor(props){
+    super(props)
 
-        ,
-        seekTime: 0
-    };
+    this.state={
+      allDetections: [
+        {id: 1, faceName: "Akassh", timestamps: [60,100,1200]},
+        {id: 2, faceName: "Visal", timestamps: [1000]},
+        {id: 3, faceName: "Nisal", timestamps: [500,100,1200, 1500]}
+      ]     // stores all the detected faces with the timestamps  ~ one time stamp each per person for now
+      ,
+      selectedPerson: null
+      ,
+      seekTime: 0
+  };
 
-    videoPlayer = null;     // videojs video player is assigned to this variable. Used to access controls of videojs
-    
-    render(){
-        return(
-            <div>                              
-                <div data-vjs-player>
-                <video ref={ node => this.videoNode = node } id="videoPlayer" className="video-js vjs-theme-fantasy" width="800" height="450" data-setup='{ "controls": true, "autoplay": false}'>
-                    <source src="/Videos/FallingInLove.mp4" type="video/mp4"/>
-                    {/* video that needs to be added to check for faces will be opened here */}
-                </video>
-                </div>
+  this.videoPlayer = null;     // videojs video player is assigned to this variable. Used to access controls of videojs
 
-                {/* to test seeking functionality using videojs options */}
-                <button onClick= {() => this.videoPlayer.currentTime(50)}>Seek 50s</button>
-                <button onClick= {() => this.videoPlayer.currentTime(700)}>Seek 700s</button>
-                <button onClick= {() => this.videoPlayer.currentTime(1400)}>Seek 1400s</button>
-                {/* <button onClick= {() => this.videoPlayer.playbackRate(5)}>Adjust Options</button> */}
-
-                {/* for every time that exists, create a button and display */}
-                {this.state.allDetections.map(person => (
-                  <div key={person.id}>
-                    <TimeCard
-                      key = {person.id}
-                      timeStamp = {person.timeStamp}
-
-                      onSeek = {() => this.seekToTime(person.timeStamp)}    // when button is pressed, go to seek time
-                    />
-
-                    {/* to test whether multiple components are created
-                    <button onClick= {() => this.videoPlayer.currentTime(1000)}>Seek 1000s</button> */}
-                  </div>
-                ))}
-
-            </div>
-        )
-    }
+  }    
 
     componentDidMount() {
         // instantiate Video.js
@@ -70,9 +42,60 @@ export default class DashBoard extends Component{
 
     
       // this method seeks the video to the specified timestamp
-    seekToTime(timeStamp){
-      console.log("I was clicked")    // to test whether function gets called on button click
-
-      this.videoPlayer.currentTime(timeStamp)   // jump to time stamp of current time-card
+    seekToTime(time){
+      // console.log("I was clicked")    // to test whether function gets called on button click
+      this.videoPlayer.currentTime(time)   // jump to time stamp of current time-card
   }
+
+  showTimeCards(person){
+    console.log(person)
+
+    this.setState({selectedPerson:person})    // updating state of selectedPerson
+  }
+
+  render(){
+    return(
+        <div>                              
+            <div data-vjs-player>
+            <video ref={ node => this.videoNode = node } id="videoPlayer" className="video-js vjs-theme-fantasy" width="800" height="450" data-setup='{ "controls": true, "autoplay": false}'>
+                <source src="/Videos/random_video.mp4" type="video/mp4"/>
+                {/* video that needs to be added to check for faces will be opened here */}
+            </video>
+            </div>
+
+            {/* to test seeking functionality using videojs options */}
+            {/* <button onClick= {() => this.videoPlayer.playbackRate(5)}>Adjust Options</button> */}
+
+            {/* display all the names of the people recognized */}
+            {this.state.allDetections.map(person => (
+              <div key={person.id}>
+
+                <Person 
+                  key = {person.id}
+                  name = {person.faceName}
+
+                  onChoose = {() => this.showTimeCards(person)}
+                  allTimestamps = {person.timestamps}     // taking all the timestamps of the relevant person
+
+                  />
+
+              </div>
+            ))}
+
+
+            {this.state.selectedPerson && this.state.selectedPerson.timestamps.map((timestamp, index) => {
+              // if a selectedPerson exists, display all Time Cards of that person
+              return (
+                <TimeCard
+                  key = {index}
+                  timestamp = {timestamp}
+
+                  onSeek = {() => this.seekToTime(timestamp)}    // when button is pressed, go to seek time
+                />
+)
+            })}
+        </div>
+    )
+}
+
 }
--- ui/src/components/timeCard.jsx
+++ ui/src/components/timeCard.jsx
@@ -2,18 +2,17 @@ import React, {Component} from "react";
 
 class TimeCard extends Component{
     state={
-        timeStamp: this.props.timeStamp
+        timestamp: this.props.timestamp
     };
 
     render(){
         return(
             <React.Fragment>
-                <div>~Image of the TimeCard~
-
-                </div>
+                <div>~Image of the TimeCard~</div>
+                
                 <button onClick = {this.props.onSeek}>
                 Time: {/* The time will be shown here */}
-                <span>{this.props.timeStamp}s</span>
+                <span>{this.props.timestamp}s</span>
                 </button>
             
             </React.Fragment>
