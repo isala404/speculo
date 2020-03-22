c6c0015c5c348454179d4a1963a35735c410575f
Dinuka <drpiyadigama@gmail.com>
2020-03-22 14:21:42 +0530

    Rearranged files and implemented routing in the app

---
+++ ui/user-dashboard/src/Routes.js
@@ -0,0 +1,12 @@
+import Home from "./views/Home.jsx";
+import DashBoard from "./views/dashboard.jsx";
+
+// data object to make code simpler
+
+// All the routes in the application are defined here
+const Routes = [
+    {name: 'Home', path:'/', Component:Home},
+    {name: 'Dashboard', path:'/dashboard', Component:DashBoard}
+];
+
+export default Routes
--- ui/user-dashboard/src/index.js
+++ ui/user-dashboard/src/index.js
@@ -1,11 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-// import Home from "./components/home";
-// import AllFaces from "./components/allFaces";
-// import SpecFaces from "./components/specFaces";
-import DashBoard from "./views/dashboard";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Route} from "react-router-dom";
 
 ReactDOM.render(
-    <DashBoard/>, document.getElementById("root")
-);
+    <BrowserRouter>
+        <Route path="/" component={App}/>
+    </BrowserRouter>,
+    document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
