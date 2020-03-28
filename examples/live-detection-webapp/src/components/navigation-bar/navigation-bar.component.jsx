af5eb8e168bf2d7877822349822d19ff481dd171
Visal Rajapakse <visalrajapakse@gmail.com>
2020-03-29 02:58:50 +0530

    implemented partially complete responsive UI for the Home Screen

--- examples/live-detection-webapp/src/components/navigation-bar/navigation-bar.component.jsx
+++ examples/live-detection-webapp/src/components/navigation-bar/navigation-bar.component.jsx
@@ -1,43 +1,45 @@
 import React from "react";
+import { Nav, Navbar} from "react-bootstrap";
 import { Logo } from "../../assets/speculo-logo";
 import "./navigation-bar.style.scss";
+import { CustomButton } from "../button/button.component";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export const NavigationBar = () => {
-
-  const navigationBarStyle = {
-    position: "fixed",
-    top: 0,
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "100%",
-    height: 80,
-    backgroundColor: "#ffffff",
-    fontWeight: "bold",
-    padding: "0px 20px",
-    boxSizing: "border-box",
-  };
-
+export const NavigationMenu = () => {
   return (
-    <div className="navigation-bar" style={navigationBarStyle}>
+    <div>
       <div>
-        <Logo /> {/*SVG component for the Logo*/}
-      </div>
-      <div
-        style={{
-          width: "30%",
-          justifyContent: "space-between",
-          display: "flex",
-          marginRight : 50,
-          fontFamily: "Montserrat",
-          fontWeight: "lighter",
-          fontSize: 20
-        }}
-      >
-        <span>home</span>
-        <span>about</span>
-        <span>contact</span>
+        <Navbar
+          className="NavigationMenu"
+          collapseOnSelect
+          expand="lg"
+          variant="dark"
+          style={navStyle}
+          fixed="top" 
+        >
+          <Navbar.Brand href="#home">
+            <Logo/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto"></Nav>
+            <Nav>
+              <Nav.Link href="#features" style={{justifyContent: "center"}}>Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link href="#pricing"><span style ={textButtonStyle} >Log In</span></Nav.Link>
+              {/* <Nav.Link href="#pricing" ><CustomButton buttonTitle="Sign Up"/></Nav.Link> */}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     </div>
   );
 };
+
+const navStyle = {
+  background: "#0B162B",
+};
+
+const textButtonStyle = {
+  fontWeight: "bold",
+}
