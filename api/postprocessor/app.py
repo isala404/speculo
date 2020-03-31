430cca9562ff5e23beb6187d48a7f35cac3761e9
akassharjun <akassharjun@gmail.com>
2020-04-01 02:02:07 +0530

    added some documention & comments

--- api/postprocessor/app.py
+++ api/postprocessor/app.py
@@ -1,8 +1,28 @@
+#!/usr/bin/env python
+
+"""app.py: A microservice for the admin panel to save new faces to the database."""
+
+__author__ = "Akassharjun Shanmugarajah"
+__version__ = "0.0.1"
+__email__ = "akassharjun@ieee.org"
+__status__ = "Testing"
+
+# standard libraries
+import os
+from random import randint
+
+# 3rd party packages
 import requests
 import json
 import numpy as np
-import PIL
 from PIL import Image, ImageDraw
+from mongoengine import Document, StringField, ListField, IntField, connect
+
+
+class Face(Document):
+	face_id = IntField(required=True)
+	face_label = StringField(max_length=50)
+	face_matrix = ListField(required=True)
 
 
 class ImagePostprocessor:
@@ -21,14 +41,13 @@ class ImagePostprocessor:
 		data = json.loads(response.text)
 		
 		if 'error' in data.keys():
-			print(data['error'])
+			print("error in detecting faces")
 			return data['error']
 		
 		return data['predictions']
 	
 	def _get_fingerprint(self, current_face):
 		# using old model
-		# body = json.dumps({'instances': np.reshape(current_face, [-1, 64, 64, 3]).tolist()})
 		body = json.dumps({'instances': np.reshape(np.array(current_face), [-1, 64, 64, 3]).tolist()})
 		
 		response = requests.post(url=self._FINGERPRINT_ENDPOINT, data=body)
@@ -36,7 +55,7 @@ class ImagePostprocessor:
 		data = json.loads(response.text)
 		
 		if 'error' in data.keys():
-			print("error in retrieving fingerprint")
+			print("error in generating fingerprint")
 			return 0
 		
 		return data['predictions']
@@ -50,8 +69,6 @@ class ImagePostprocessor:
 		
 		boxes = self._get_faces(resized_image=resized_im)
 		
-		print(boxes)
-		
 		for top, left, bottom, right in boxes:
 			# Setting the points for cropped image
 			top = int(top * height / self._SIZE)
@@ -60,16 +77,36 @@ class ImagePostprocessor:
 			left = int(left * width / self._SIZE)
 			
 			# Cropped image of above dimension
-			# (It will not change orginal image)
 			face = im.crop((left, top, right, bottom))
 			
-			face = face.resize(self._FINGERPRINT_SIZE,  Image.ANTIALIAS)
+			# resize the cropped image to the required size
+			face = face.resize(self._FINGERPRINT_SIZE, Image.ANTIALIAS)
 			
+			# to draw a red rectangle outline around the face
 			image_drawer = ImageDraw.Draw(face)
 			image_drawer.rectangle(((left, top), (right, bottom)), fill=None, outline="red")
 			
 			fingerprint = self._get_fingerprint(current_face=face)
 			
-			print(fingerprint)
+			# connect to the mongodb server
+			connect(
+				db='face',
+				username='user',
+				password='4313Samadhi',
+				host='mongodb+srv://user:4313Samadhi@cluster0-jqb4b.mongodb.net/speculo'
+			)
+			
+			# retrieve the label of the face from the filename
+			face_label = self.filename.split('.')[0]
+			
+			# instantiate an object with the face data
+			face_data = Face(face_id='{}'.format(randint(1, 101)), face_label=face_label, face_matrix=fingerprint)
+			
+			# save the object
+			face_data.save()
+			
+			# close the image file and remove it from the images directory
+			im.close()
+			os.remove("./images/{}".format(self.filename))
 			
 			break
--- api/postprocessor/rest_api.py
+++ api/postprocessor/rest_api.py
@@ -1,20 +1,21 @@
-import json
+# standard library
 import os
+
+# 3rd party packages
+import json
 from aiohttp import web
-from multidict import MultiDict
+
+# local package
 from app import ImagePostprocessor
 
 
 async def save_image(request):
 	reader = await request.multipart()
 	
-	# /!\ Don't forget to validate your inputs /!\
-	
-	# reader.next() will `yield` the fields of your form
+	# reader.next() will `yield` the fields of the form
 	field = await reader.next()
 	filename = field.filename
 	
-	# You cannot rely on Content-Length if transfer is chunked.
 	size = 0
 	with open((os.getcwd() + '/images/' + filename), 'wb') as f:
 		while True:
@@ -33,7 +34,9 @@ async def save_face_data(request):
 		
 		ImagePostprocessor(filename=filename).save_face_data()
 		
-		return web.Response(text='{} successfully saved'''.format(filename))
+		response_obj = {'status': 'success', 'message': str('{} successfully saved'''.format(filename))}
+		
+		return web.Response(text=json.dumps(response_obj), status=200)
 	
 	except Exception as e:
 		print(e)
@@ -44,26 +47,26 @@ async def save_face_data(request):
 		return web.Response(text=json.dumps(response_obj), status=500)
 
 
-async def edit_face_data(request):
-	try:
-		filename = await save_image(request)
-		
-		return web.Response(text='{} successfully edited'''.format(filename))
-	
-	except Exception as e:
-		print(e)
-		# Failed path where name is not set
-		response_obj = {'status': 'failed', 'reason': str(e)}
-		
-		# return failed with a status code of 500 i.e. 'Server Error'
-		return web.Response(text=json.dumps(response_obj), status=500)
+# async def edit_face_data(request):
+# 	try:
+# 		filename = await save_image(request)
+#
+# 		return web.Response(text='{} successfully edited'''.format(filename))
+#
+# 	except Exception as e:
+# 		print(e)
+# 		# Failed path where name is not set
+# 		response_obj = {'status': 'failed', 'reason': str(e)}
+#
+# 		# return failed with a status code of 500 i.e. 'Server Error'
+# 		return web.Response(text=json.dumps(response_obj), status=500)
 
 
 app = web.Application()
 
 routes = [
-	web.post('/api/v1/postprocess/create', save_face_data),
-	web.post('/api/v1/postprocess/edit', edit_face_data),
+	web.post('/api/v1/postprocess', save_face_data),
+	# web.put('/api/v1/postprocess', edit_face_data),
 ]
 
 app.add_routes(routes)
