c40599e5bd335230fe8b561402bd4dbcd0ad1d7a
akassharjun <akassharjun@gmail.com>
2020-04-01 23:24:25 +0530

    initial commit for face filtering logic

---
+++ ui/user-dashboard/src/faceFilter.js
@@ -0,0 +1,14 @@
+let allDetections = [
+    {id: 1, faceName: "Akassh", timestamps: [60, 100, 1200]},
+    {id: 2, faceName: "Visal", timestamps: [1000]},
+    {id: 3, faceName: "Nisal", timestamps: [100, 500, 1200, 1500]}
+]
+
+// timestamps will be in milliseconds
+
+// milliseconds for this scenario
+let selectedTime = 60
+
+allDetections.forEach((detection) => {
+    
+})
