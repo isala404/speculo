1e2438ccd785b91fdf05660210f8191380051173
Dinuka <drpiyadigama@gmail.com>
2020-04-09 21:05:02 +0530

    Added env variable as the endpoint

--- ui/user-dashboard/src/variables/constants.jsx
+++ ui/user-dashboard/src/variables/constants.jsx
@@ -1,2 +1,2 @@
-export var speculoBackend = "http://localhost:7000";            // localhost testing purposes
-// export var speculoBackend = "https://speculo-server.herokuapp.com";         // hosted service testing purposes
+export const speculoBackend = process.env.ENDPOINT;            // localhost testing purposes
+// export const speculoBackend = "https://speculo-server.herokuapp.com";         // hosted service testing purposes
