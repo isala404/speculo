269897ccfd0dae49d0665130343dea56ce843147
kushanshamika <shamikakushan@gmail.com>
2020-04-11 22:44:02 +0530

    Fixed mongodb connection warning

--- api/gateway/config/database.js
+++ api/gateway/config/database.js
@@ -1,6 +1,6 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
 const mongoDB = 'mongodb://localhost/speculo';
-mongoose.connect(mongoDB);
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
