b1b46c4ad645dcd8049e30b97b3456f67a35d6a7
Isala Piyarisi <code@isala.me>
2020-04-11 14:10:24 +0530

    Minor code Refactoring

--- ui/user-dashboard/src/services/DetectionsManagement.jsx
+++ ui/user-dashboard/src/services/DetectionsManagement.jsx
@@ -2,17 +2,17 @@ import axios from "axios";
 import {detectionsEndpoint} from "../endpoints";
 
 export async function retrieveAllDetections() {
-    try {
-        //get allDetections data
-        return await axios.get(detectionsEndpoint,
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    'Access-Control-Allow-Origin': '*',
-                }
-            });
-    } catch (e) {
-        console.log(e)
-        return null
-    }
+    const res = await axios.get(detectionsEndpoint, {
+        headers: {
+            "Content-Type": "application/json",
+            'Access-Control-Allow-Origin': '*',
+        }
+    });
+    return res.data.map(detection => {
+        return {
+            id: detection._id,
+            faceName: detection.name,
+            timestamps: detection.timestamps
+        };
+    });
 }
