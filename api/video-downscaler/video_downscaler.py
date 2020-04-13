31b7bca9abfeaba9b58197519ee866b09b512ee1
Akassharjun Shanmugarajah <akassharjun@gmail.com>
2020-04-13 20:22:50 +0530

    completed video downscaler

--- api/video-downscaler/video_downscaler.py
+++ api/video-downscaler/video_downscaler.py
@@ -13,19 +13,18 @@ class VideoDownscaler:
 			print(count, count % 2 == 0)
 			if count % 2 == 0:
 				subprocess.call(
-					f"ffmpeg -y -i ./videos/{self.filename}.mp4 -vf mpdecimate,setpts=N/FRAME_RATE/TB ./videos/out.mp4",
+					f"ffmpeg -y -i ./videos/{self.filename}.mp4 -vf mpdecimate,setpts=N/FRAME_RATE/TB ./videos/{self.filename}_out.mp4",
 					shell=True
 				)
 				
 				os.remove(f'./videos/{self.filename}.mp4')
 			else:
 				subprocess.call(
-					f"ffmpeg -y -i ./videos/out.mp4 -vf mpdecimate,setpts=N/FRAME_RATE/TB ./videos/{self.filename}.mp4",
+					f"ffmpeg -y -i ./videos/{self.filename}_out.mp4 -vf mpdecimate,setpts=N/FRAME_RATE/TB ./videos/{self.filename}.mp4",
 					shell=True
 				)
 				
-				os.remove(f'./videos/out.mp4')
+				os.remove(f'./videos/{self.filename}_out.mp4')
 		
-		os.rename(f'./videos/out.mp4', f'./videos/{self.filename}.mp4')
+		os.rename(f'./videos/{self.filename}_out.mp4', f'./videos/{self.filename}.mp4')
 		
-		return True
