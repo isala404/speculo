7cb7fa455bb44b66ed59c249135fc24254df53eb
kushanshamika <shamikakushan@gmail.com>
2020-04-17 14:44:15 +0000

    Enabling CORS in gateway

--- api/gateway/index.js
+++ api/gateway/index.js
@@ -21,6 +21,12 @@ mongoose.connection.on(
 app.use(logger("dev"));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 app.get("/", function (req, res) {
     res.send("Welcome to Speculo Gateway");
 });
