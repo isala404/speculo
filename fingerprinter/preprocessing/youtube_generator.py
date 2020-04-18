8b27f4829f8089a040666ffef219c21b532e63f9
Isala Piyarisi <code@isala.me>
2020-04-18 17:16:38 +0530

    Fixed Face mismatch

--- fingerprinter/preprocessing/youtube_generator.py
+++ fingerprinter/preprocessing/youtube_generator.py
@@ -2,21 +2,23 @@ import os
 import pickle
 import random
 from tqdm import tqdm
-from PIL import Image
-import numpy as np
+
+BASE_DIR = "/mnt/hdd/Projects/SDGP/Speculo/fingerprinter"
 
 data = []
-if not os.path.isdir("../dataset_processed"):
+if not os.path.isdir(os.path.join(BASE_DIR, "dataset_processed")):
     raise FileNotFoundError("fingerprinter/dataset_processed was not found")
-for person in tqdm(os.listdir("../dataset_processed")):
-    for Y in os.listdir(os.path.join("../dataset_processed", person, "Y")):
-        for X in os.listdir(os.path.join("../dataset_processed", person, "X")):
-            data.append([os.path.join("../dataset_processed", person, "X", X),
-                         os.path.join("../dataset_processed", person, "Y", Y)])
+for person in tqdm(os.listdir(os.path.join(BASE_DIR, "dataset_processed"))):
+    for Y in os.listdir(os.path.join(BASE_DIR, "dataset_processed", person, "Y")):
+        for X in os.listdir(os.path.join(BASE_DIR, "dataset_processed", person, "X")):
+            if X.split(".")[0].split("_")[-1] == Y.split(".")[0].split("_")[-1]:
+                data.append([os.path.join(BASE_DIR, "dataset_processed", person, "X", X),
+                             os.path.join(BASE_DIR, "dataset_processed", person, "Y", Y)])
+
 
 random.shuffle(data)
-if not os.path.isdir("../dataset"):
-    os.makedirs("../dataset")
-file = open('../dataset/youtube_data_map.pkl', 'wb')
+if not os.path.isdir(os.path.join(BASE_DIR, "dataset")):
+    os.makedirs(os.path.join(BASE_DIR, "dataset"))
+file = open(os.path.join(BASE_DIR, "dataset", "youtube_data_map.pkl"), 'wb')
 pickle.dump(data, file)
 file.close()
