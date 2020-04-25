44c9dea60a1f8ff88578f033b957ef04cdd13c52
nizzlo <nisalsamaranayake@gmail.com>
2020-04-25 12:50:57 +0530

    Addind admin registration to the UI

--- ui/user-dashboard/src/components/login/register.jsx
+++ ui/user-dashboard/src/components/login/register.jsx
@@ -86,6 +86,52 @@ export class Register extends React.Component {
         }
     }
 
+    handleAdminRegistration = e => {
+        e.preventDefault()
+        console.log("[Client] -User Registration")
+        console.log(this.state)
+
+        const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+
+        if (this.state.name != "" && this.state.password != "" && this.state.email != "") {
+            if (expression.test(String(this.state.email).toLowerCase()) && this.state.password==this.state.passwordCon) {
+                axios.post('http://ec2-18-217-163-34.us-east-2.compute.amazonaws.com:3000/admin/register', this.state)
+                    .then(response => {
+                        console.log(response)
+                        const parsedResponse = JSON.parse(JSON.stringify(response));
+                        //console.log(parsedResponse.data.status)
+
+                        if (parsedResponse.data.status === "success") {
+                            //console.log(parsedResponse.status)
+                            alert(parsedResponse.data.message);
+                            window.location.href = "/login";
+                        }
+                        else {
+                            console.log(parsedResponse.data.status)
+                            alert(parsedResponse.data.message);
+                            
+                        }
+                        //console.log(response)
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+            } else {
+                if (!expression.test(String(this.state.email).toLowerCase())){
+                    alert("Invalid Email Address.");
+                }
+                if (this.state.password!=this.state.passwordCon){
+                    alert("Passwords Doesn't Match.");
+                    
+                }
+                
+            }
+        } else {
+            alert("All Fields Required!");
+        }
+    }
+
+
     render() {
         return (
             <>
@@ -128,7 +174,10 @@ export class Register extends React.Component {
 
                     <div className="footer">
                         <button type="button" className="btn" onClick={this.handleUserRegistration}>
-                            Register
+                            User Register
+                        </button>
+                        <button type="button" className="btn" onClick={this.handleAdminRegistration}>
+                            Admin Register
                         </button>
                     </div>
 
