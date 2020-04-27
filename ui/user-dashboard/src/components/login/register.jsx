dcbec4af56f57a17f569348bf4f0a20584784250
nizzlo <nisalsamaranayake@gmail.com>
2020-04-27 08:18:55 +0530

    adding password strength checker

--- ui/user-dashboard/src/components/login/register.jsx
+++ ui/user-dashboard/src/components/login/register.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 import { NavigationMenu } from "../navigation-bar/navigation-bar.component";
 import { Footer } from "../home-footer/footer.component";
-import  { Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 //import {Animation} from "./animation";
 
 
@@ -51,7 +51,32 @@ export class Register extends React.Component {
         const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 
         if (this.state.name != "" && this.state.password != "" && this.state.email != "") {
-            if (expression.test(String(this.state.email).toLowerCase()) && this.state.password==this.state.passwordCon) {
+            if (expression.test(String(this.state.email).toLowerCase()) && this.state.password == this.state.passwordCon) {
+
+                if (!RegExp(/[0-9]/).test(this.state.password) || 
+                !RegExp(/[a-z]/).test(this.state.password) || 
+                !RegExp(/[!#@$%^&*)(+=._-]/).test(this.state.password)) {
+                    var strength = 0;
+                    if (RegExp(/[0-9]/).test(this.state.password)) {
+                        strength += 1
+                    }
+                    if (RegExp(/[a-z]/).test(this.state.password)) {
+                        strength += 1
+                    }
+                    if (RegExp(/[!#@$%^&*)(+=._-]/).test(this.state.password)) {
+                        strength += 1
+                    }
+
+                    if (strength >= 2) {
+                        alert("Mid Range Password")
+                    } else {
+                        alert("Weak Password")
+                    }
+                }else{
+                    alert("Strong Password")
+                }
+
+                if(window.confirm("Are you sure to submit?"))
                 axios.post('http://speculo.isala.me/api/v1/user/register', this.state)
                     .then(response => {
                         console.log(response)
@@ -66,22 +91,23 @@ export class Register extends React.Component {
                         else {
                             console.log(parsedResponse.data.status)
                             alert(parsedResponse.data.message);
-                            
+
                         }
                         //console.log(response)
                     })
                     .catch(error => {
                         console.log(error)
                     })
+
             } else {
-                if (!expression.test(String(this.state.email).toLowerCase())){
+                if (!expression.test(String(this.state.email).toLowerCase())) {
                     alert("Invalid Email Address.");
                 }
-                if (this.state.password!=this.state.passwordCon){
+                if (this.state.password != this.state.passwordCon) {
                     alert("Passwords Doesn't Match.");
-                    
+
                 }
-                
+
             }
         } else {
             alert("All Fields Required!");
@@ -96,7 +122,32 @@ export class Register extends React.Component {
         const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 
         if (this.state.name != "" && this.state.password != "" && this.state.email != "") {
-            if (expression.test(String(this.state.email).toLowerCase()) && this.state.password==this.state.passwordCon) {
+            if (expression.test(String(this.state.email).toLowerCase()) && this.state.password == this.state.passwordCon) {
+
+                if (!RegExp(/[0-9]/).test(this.state.password) || 
+                !RegExp(/[a-z]/).test(this.state.password) || 
+                !RegExp(/[!#@$%^&*)(+=._-]/).test(this.state.password)) {
+                    var strength = 0;
+                    if (RegExp(/[0-9]/).test(this.state.password)) {
+                        strength += 1
+                    }
+                    if (RegExp(/[a-z]/).test(this.state.password)) {
+                        strength += 1
+                    }
+                    if (RegExp(/[!#@$%^&*)(+=._-]/).test(this.state.password)) {
+                        strength += 1
+                    }
+
+                    if (strength >= 2) {
+                        alert("Mid Range Password")
+                    } else {
+                        alert("Weak Password")
+                    }
+                }else{
+                    alert("Strong Password")
+                }
+
+                if(window.confirm("Are you sure to submit?"))
                 axios.post('http://speculo.isala.me/api/v1/admin/register', this.state)
                     .then(response => {
                         console.log(response)
@@ -111,7 +162,7 @@ export class Register extends React.Component {
                         else {
                             console.log(parsedResponse.data.status)
                             alert(parsedResponse.data.message);
-                            
+
                         }
                         //console.log(response)
                     })
@@ -119,14 +170,14 @@ export class Register extends React.Component {
                         console.log(error)
                     })
             } else {
-                if (!expression.test(String(this.state.email).toLowerCase())){
+                if (!expression.test(String(this.state.email).toLowerCase())) {
                     alert("Invalid Email Address.");
                 }
-                if (this.state.password!=this.state.passwordCon){
+                if (this.state.password != this.state.passwordCon) {
                     alert("Passwords Doesn't Match.");
-                    
+
                 }
-                
+
             }
         } else {
             alert("All Fields Required!");
@@ -141,7 +192,7 @@ export class Register extends React.Component {
                 <div style={{ background: "#000000" }}>
                     <NavigationMenu />
                 </div>
-                <div className="base-container" ref={this.props.containerRef} style={{ marginTop:"10em", marginBottom:"10em" }}>
+                <div className="base-container" ref={this.props.containerRef} style={{ marginTop: "10em", marginBottom: "10em" }}>
                     <div className="header">Register</div>
                     <div className="content">
 
