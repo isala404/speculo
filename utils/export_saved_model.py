d017ad5be637166badeb13ed85a65e48112b0015
Isala Piyarisi <code@isala.me>
2020-04-29 18:22:41 +0530

    Updated model upload script

--- utils/export_saved_model.py
+++ utils/export_saved_model.py
@@ -1,10 +1,15 @@
 import os
 import tensorflow as tf
+from tensorflow_core.python.keras import Model
 
-version = 11
+version = 12
+encoder_only = False
 
 MODEL_DIR = "../models/fingerprinter/models"
 model = tf.keras.models.load_model(f"../models/fingerprinter/models/{version}/Model-v{version}.h5")
+
+if encoder_only:
+    model = Model(inputs=model.input, outputs=model.get_layer("latent_space").output)
 
 export_path = os.path.join(MODEL_DIR, str(version))
 print('export_path = {}\n'.format(export_path))
@@ -20,4 +25,4 @@ tf.keras.models.save_model(
     options=None
 )
 
-print('\nSaved model:')
+print('\nModel Saved to :', os.path.abspath(export_path))
