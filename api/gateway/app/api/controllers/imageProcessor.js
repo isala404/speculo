6b8e5bff8faf4a33f2412c2891a2c54945456f7f
kushanshamika <shamikakushan@gmail.com>
2020-04-30 18:41:52 +0530

    Added missing end points

--- api/gateway/app/api/controllers/imageProcessor.js
+++ api/gateway/app/api/controllers/imageProcessor.js
@@ -24,5 +24,43 @@ module.exports = {
         .catch(error =>{
             res.status(400).send({'status':'Bad Request', 'error' : error.meessage})
         })
-    }
+    },
+
+    get_coordinates: function(req, res, next){
+
+      const file = req.files;
+
+      let form = new FormData();
+      file.forEach(element => {
+        form.append('image', element.buffer, element.originalname);
+      });
+    
+    
+      api
+      .post('api'+req.path, form, {'maxContentLength': Infinity, 'maxBodyLength': Infinity, headers:{'Content-Type': `multipart/form-data; boundary=${form._boundary}`}}).then(resp=>{
+        res.send(resp.data)
+      })
+      .catch(error =>{
+          res.status(400).send({'status':'Bad Request', 'error' : error.meessage})
+      })
+  },
+
+  generate_fingerprint: function(req, res, next){
+
+    const file = req.files;
+
+    let form = new FormData();
+    file.forEach(element => {
+      form.append('image', element.buffer, element.originalname);
+    });
+  
+  
+    api
+    .post('api'+req.path, form, {'maxContentLength': Infinity, 'maxBodyLength': Infinity, headers:{'Content-Type': `multipart/form-data; boundary=${form._boundary}`}}).then(resp=>{
+      res.send(resp.data)
+    })
+    .catch(error =>{
+        res.status(400).send({'status':'Bad Request', 'error' : error.meessage})
+    })
+},
 }
