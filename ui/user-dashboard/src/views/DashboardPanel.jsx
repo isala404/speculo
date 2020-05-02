4a5034eb80fe318575f7c3f84f8afca2e9a21ef0
Dinuka <drpiyadigama@gmail.com>
2020-05-03 01:43:12 +0530

    Re-managed state changes based on completion status of the POST request

--- ui/user-dashboard/src/views/DashboardPanel.jsx
+++ ui/user-dashboard/src/views/DashboardPanel.jsx
@@ -8,12 +8,16 @@ import "../components/upload-face-footage/upload.scss";
 class DashboardPanel extends Component {
   constructor(props){
     super(props);
+
     this.state={
-      video: null
+      video: null,
+      responseDetections: null
     };
   }
 
 render(){
+  // const {responseDetections} =  null
+
 
   // const Input = ({ accept, onFiles, files, getFilesFromEvent }) => {
   //   const text = files.length > 0 ? "Add more files" : "Choose files";
@@ -56,35 +60,46 @@ render(){
           }
         
         // called every time a file's `status` changes
-        const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
+        const handleChangeStatus = ({ meta, file, xhr }, status) => {
+          console.log(status, meta, file)
+
+          if (status === 'done') {
+            console.log(xhr.response);
+
+            this.setState({
+              responseDetections: xhr.response,
+              video: URL.createObjectURL(file)
+            });
+
+            // set this response to allDetections array in component
+            // once set, make sure to turn processing boolean false in dashboard.jsx
+          }
+
+            // if (status === 'uploading'){
+            //   this.setState( {video: URL.createObjectURL(file)});
+            // }
+        }
         
         // receives array of files that are done uploading when submit button is clicked
-        const handleSubmit = (files, allFiles) => {
-          const file = files[0].file;
-          
-          // console.log(files[0]);
-          // const objURL = URL.createObjectURL(files[0].file);
-          // console.log(objURL);
-          // Save data to sessionStorage
-          // sessionStorage.setItem('videoURL', JSON.stringify({src: URL.createObjectURL(file), type: file.type}));
+        // const handleSubmit = (files, allFiles) => {
+        //   // console.log(files);
 
-          this.setState( {video: URL.createObjectURL(file)});
+        //   // const file = files[0].file;
+        //   // this.setState( {video: URL.createObjectURL(file)});                                                                                                                                                                 
 
-
-          // call function to wait for a response and only if a response is received changed, 
-          // 
-
-          allFiles.forEach(f => f.remove())
-        }
+        //   allFiles.forEach(f => f.remove())
+        // }
         
         return (
             <Dropzone
                 getUploadParams={getUploadParams}
                 onChangeStatus={handleChangeStatus}
-                onSubmit={handleSubmit}
+                // onSubmit={handleSubmit}
                 accept="video/*"      // allow only video files to be uploaded
                 maxFiles = "1"
-                inputContent={"Drag a video file or Click to browse"}
+                inputContent = "Drag a video file or Click to browse"
+                // submitButtonContent = "Display Detections"
+                submitButtonDisabled="true"
             />
           )
     }
@@ -92,16 +107,23 @@ render(){
     
     return(
         <div>
+
           {/* <MyVideoUploader /> */}
 
             {!this.state.video &&
               <MyVideoUploader />}
 
+        {/* show processing gif with person card loading templates here, untill displaying video */}
+{/* use loader screen for entire Dashboard component???? */}
 
+{/* use the timeoutfunction to set the detections in the backend */}
             {this.state.video && 
             <Dashboard 
               videoSRC = {this.state.video}
-            />}
+            />
+            }
+
+            {/* {this.state.video && console.log(this.state.responseDetections)} */}
 
              {/* <video width="320" height="240" controls>
                <source src={this.state.video} type="video/mp4"/>
