31d981d71378c95361bc0691643811019c959110
Visal Rajapakse <visalrajapakse@gmail.com>
2020-06-29 23:39:59 +0530

    fixed dashboard detected person div height

--- ui/user-dashboard/src/App.js
+++ ui/user-dashboard/src/App.js
@@ -13,23 +13,23 @@ const App = () => {
       <div className="App">
         <NavigationMenu />
         {Routes.map(({ path, Component }) => (
-            <Route key={path} exact path={path}>
-              {/* http://reactcommunity.org/react-transition-group/with-react-router.
+          <Route key={path} exact path={path}>
+            {/* http://reactcommunity.org/react-transition-group/with-react-router.
             use of CSSTransition to animate page routing */}
-              {({ match }) => (
-                <CSSTransition
-                  in={match != null}
-                  timeout={1000}
-                  classNames="view"
-                  unmountOnExit
-                >
-                  <div className="view">
-                    {/* rendering the component onto the div */}
-                    <Component />
-                  </div>
-                </CSSTransition>
-              )}
-            </Route>
+            {({ match }) => (
+              <CSSTransition
+                in={match != null}
+                timeout={1000}
+                classNames="view"
+                unmountOnExit
+              >
+                <div className="view">
+                  {/* rendering the component onto the div */}
+                  <Component />
+                </div>
+              </CSSTransition>
+            )}
+          </Route>
         ))}
       </div>
     </>
